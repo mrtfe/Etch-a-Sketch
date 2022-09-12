7f6b5162da888abe62cc1cb81e8e4fee84e08e8e
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-09-12 21:18:23 +0300

    cleanup

--- Etch-a-Sketch-/script.js
+++ Etch-a-Sketch-/script.js
@@ -1,23 +1,21 @@
 /* eslint-disable no-plusplus */
-const BOX_CONTAINER = document.querySelector(".sketch-box__container");
+const GRID_CONTAINER = document.querySelector(".sketch-box__container");
 const CLEAR_BTN = document.querySelector(".clear-btn");
 const APPLY_BTN = document.querySelector(".apply-btn");
 
 const slider = document.getElementById("myRange");
 const output = document.querySelector("p");
 
-const gridSize = slider.value * slider.value;
-
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
-}
-
 function createGrid() {
-  BOX_CONTAINER.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`;
-  BOX_CONTAINER.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`;
-  for (let i = 0; i < gridSize; i++) {
+  // clear grid
+  GRID_CONTAINER.innerHTML = "";
+
+  // create grid template based on slider option
+  GRID_CONTAINER.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`;
+  GRID_CONTAINER.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`;
+
+  // create grid
+  for (let i = 0; i < slider.value * slider.value; i++) {
     const square = document.createElement("div");
     square.classList.add("one-box");
     square.style.border = "1px solid black";
@@ -25,23 +23,17 @@ function createGrid() {
     square.addEventListener("mouseover", () => {
       square.style.backgroundColor = "grey";
     });
-    BOX_CONTAINER.appendChild(square);
+    GRID_CONTAINER.appendChild(square);
   }
 }
 
+// show slider values
 slider.oninput = () => {
   output.innerHTML = `${slider.value} x ${slider.value}`;
 };
 
-function clearDeck() {
-  removeAllChildNodes(BOX_CONTAINER);
-  createGrid();
-}
+APPLY_BTN.addEventListener("click", createGrid);
+CLEAR_BTN.addEventListener("click", createGrid);
 
-APPLY_BTN.addEventListener("click", () => {
-  removeAllChildNodes(BOX_CONTAINER);
-  createGrid();
-});
-CLEAR_BTN.addEventListener("click", clearDeck);
-
+// initialize
 createGrid();
