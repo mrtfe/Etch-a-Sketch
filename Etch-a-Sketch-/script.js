c04e28b75ed9f9ae5e08fa684a37bc8bed9ddfdc
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-09-14 19:25:42 +0300

    feat: :sparkles: random color button added

--- Etch-a-Sketch-/script.js
+++ Etch-a-Sketch-/script.js
@@ -2,7 +2,7 @@
 const GRID_CONTAINER = document.querySelector(".sketch-box__container");
 const CLEAR_BTN = document.querySelector(".clear-btn");
 const APPLY_BTN = document.querySelector(".apply-btn");
-// const RANDOM_COLOR_BTN = document.getElementById("random-color-btn");
+const RANDOM_COLOR_BTN = document.getElementById("random-color-btn");
 
 const slider = document.getElementById("myRange");
 const output = document.querySelector("p");
@@ -15,6 +15,7 @@ function createGrid() {
   GRID_CONTAINER.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`;
   GRID_CONTAINER.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`;
 
+  const color = "grey";
   // create grid
   for (let i = 0; i < slider.value * slider.value; i++) {
     const square = document.createElement("div");
@@ -22,21 +23,36 @@ function createGrid() {
     square.style.border = "1px solid black";
     square.style.backgroundColor = "white";
     square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "grey";
+      square.style.backgroundColor = `${color}`;
     });
     GRID_CONTAINER.appendChild(square);
   }
 }
 
-// generate random color
-// function getRandomColor() {
-//   const letters = "0123456789ABCDEF";
-//   let color = "#";
-//   for (let i = 0; i < 6; i++) {
-//     color += letters[Math.floor(Math.random() * 16)];
-//   }
-//   return color;
-// }
+function drawRandomColor() {
+  // clear grid
+  GRID_CONTAINER.innerHTML = "";
+  // create grid template based on slider option
+  GRID_CONTAINER.style.gridTemplateColumns = `repeat(${slider.value}, 1fr)`;
+  GRID_CONTAINER.style.gridTemplateRows = `repeat(${slider.value}, 1fr)`;
+  // generate random color
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  // draw random color
+  for (let i = 0; i < slider.value * slider.value; i++) {
+    const square = document.createElement("div");
+    square.classList.add("one-box");
+    square.style.border = "1px solid black";
+    square.style.backgroundColor = "white";
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = `${color}`;
+    });
+    GRID_CONTAINER.appendChild(square);
+  }
+}
 
 // show slider values
 slider.oninput = () => {
@@ -45,7 +61,7 @@ slider.oninput = () => {
 
 APPLY_BTN.addEventListener("click", createGrid);
 CLEAR_BTN.addEventListener("click", createGrid);
-// RANDOM_COLOR_BTN.addEventListener("click", getRandomColor);
+RANDOM_COLOR_BTN.addEventListener("click", drawRandomColor);
 
 // initialize
 createGrid();
